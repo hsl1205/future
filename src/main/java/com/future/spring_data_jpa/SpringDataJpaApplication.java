b6e351c5b34a294d309395f0971f1bea852c4b4e
huangsl1205 <huangsl@augurit.com>
2020-12-24 14:23:33 +0800

    测试回退

--- src/main/java/com/future/spring_data_jpa/SpringDataJpaApplication.java
+++ src/main/java/com/future/spring_data_jpa/SpringDataJpaApplication.java
@@ -15,5 +15,4 @@ public class SpringDataJpaApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaApplication.class, args);
     }
-
 }
--- src/test/java/com/future/spring_data_jpa/SpringDataJpaApplicationTests.java
+++ src/test/java/com/future/spring_data_jpa/SpringDataJpaApplicationTests.java
@@ -1,5 +1,6 @@
 package com.future.spring_data_jpa;
 
+import com.future.spring_data_jpa.pdf.untemplates.PdfTemplates;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,9 @@ class SpringDataJpaApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void pdfDemo() {
+        PdfTemplates pdfTemplates = new PdfTemplates();
+        pdfTemplates.demo();
+    }
 }
